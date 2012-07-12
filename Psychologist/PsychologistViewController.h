53096afe9784d6280c195d4daea9cde204900843
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-07-12 22:11:08 +0900

    Initial Commit

---
+++ Psychologist/PsychologistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PsychologistViewController.h
+//  Psychologist
+//
+//  Created by 式正 鍋田 on 12/07/12.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistViewController : UIViewController
+
+@end
---
+++ Psychologist/PsychologistViewController.m
@@ -0,0 +1,34 @@
+//
+//  PsychologistViewController.m
+//  Psychologist
+//
+//  Created by 式正 鍋田 on 12/07/12.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import "PsychologistViewController.h"
+
+@interface PsychologistViewController ()
+
+@end
+
+@implementation PsychologistViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
