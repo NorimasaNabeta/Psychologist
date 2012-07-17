cfdefbf0b1ec9fb5b9d90b5f4718ac2bb309c1b5
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-07-17 17:54:17 +0900

    revise for the iPhone emulation mode on the iPad. struts and spring and button alignment and RotatableViewController.[hm]

---
+++ Psychologist/RotatableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RotatableViewController.h
+//  Psychologist
+//
+//  Created by 式正 鍋田 on 12/07/17.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatableViewController : UIViewController
+
+@end
---
+++ Psychologist/RotatableViewController.m
@@ -0,0 +1,23 @@
+//
+//  RotatableViewController.m
+//  Psychologist
+//
+//  Created by 式正 鍋田 on 12/07/17.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import "RotatableViewController.h"
+
+@interface RotatableViewController ()
+
+@end
+
+@implementation RotatableViewController
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
